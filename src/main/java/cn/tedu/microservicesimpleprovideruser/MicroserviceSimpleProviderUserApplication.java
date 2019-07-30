2b6320eea83137b8c51e793d3c18709ef37de7c0
zhanghao <vip420279007@163.com>
2019-07-30 16:18:16 +0800

    服务注册到需认证的eureka集群中

--- src/main/java/cn/tedu/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
+++ src/main/java/cn/tedu/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
@@ -2,8 +2,10 @@ package cn.tedu.microservicesimpleprovideruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceSimpleProviderUserApplication {
 
     public static void main(String[] args) {
